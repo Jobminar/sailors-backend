4011ed55c3d3dbe21d895ebbcb14a538e1c175c9
kamalakar778 <kamalakar579@gmail.com>
2024-08-16 13:15:32 +0530

    worked on signup signin verifyotp create newpassword APIs

--- server/admin/admin-controller/admin.controller.js
+++ server/admin/admin-controller/admin.controller.js
@@ -3,11 +3,12 @@ import jwt from "jsonwebtoken";
 import randomstring from "randomstring";
 import path from "path";
 import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
 
 const adminController = {
   //Create User _______________________________________
 
-  signupAdmin: async (req, res) => {
+  register: async (req, res) => {
     const { email } = req.body;
     if (!email) {
       return res.status(401).json({ message: "check user credentials" });
@@ -19,17 +20,17 @@ const adminController = {
     try {
       const existing = await Admin.findOne({ email });
       if (existing) {
-        const user = await Admin.findOneAndUpdate(
-          { email },
-          { otp, otpExpiry },
-          { upsert: true, new: true }
-        );
-
+        //redirect to login
         return res
           .status(200)
-          .json({ message: "OTP sent successfully", otp: otp });
+          .json({ message: "user already exists", email: email });
       }
 
+      // const user = await Admin.findOneAndUpdate(
+      //   { email },
+      //   { otp, otpExpiry },
+      //   { upsert: true, new: true }
+      // );
       const newAdmin = new Admin({
         email,
         otp,
@@ -39,48 +40,16 @@ const adminController = {
 
       res
         .status(201)
-        .json({ message: "User created successfully", user: newAdmin });
+        .json({
+          message: "Administrator created successfully",
+          admin: newAdmin
+        });
     } catch (error) {
       console.error("Error generating OTP:", error);
       return res.status(500).json({ message: "Internal Server Error" });
     }
   },
 
-  //New Password _____________________________________
-
-  newPassword: async(req,res) => {
-    const { password, confirmPassword } = req.body;
-    if (password !== confirmPassword) {
-      return res.status(401).json({ message: "password differs" });
-    }
-
-    try {
-      const existing = await Admin.findOne({ email });
-      if (existing) {
-        const admin = await Admin.findOneAndUpdate(
-          { password },
-        );
-
-        return res
-          .status(200)
-          .json({ message: "OTP sent successfully", otp: otp });
-      }
-
-      const newAdmin = new Admin({
-        email,
-        otp,
-        otpExpiry
-      });
-      await newAdmin.save();
-
-      res
-        .status(201)
-        .json({ message: "User created successfully", user: newAdmin });
-    } catch (error) {
-      console.error("Error generating OTP:", error);
-      return res.status(500).json({ message: "Internal Server Error" });
-    }
-  },
   //Verify OTP _______________________________________
 
   verifyOtp: async (req, res) => {
@@ -90,8 +59,7 @@ const adminController = {
     }
 
     try {
-      const admin = await Admin.findOne({ mobileNumber, otp });
-      console.log("admin", admin);
+      const admin = await Admin.findOne({ email });
       if (!admin) {
         return res.status(404).json({ message: "Invalid OTP or expired OTP" });
       }
@@ -103,19 +71,95 @@ const adminController = {
       admin.otpExpiry = null;
       await admin.save();
 
-      console.log("user.mobileNumber", admin);
-
-      // Generate JWT
-      const token = jwt.sign({ adminId: admin._id }, process.env.JWT_SECRET_ADMIN, {
-        expiresIn: "1h"
-      });
+      console.log("adim email", admin);
 
       return res.status(200).json({
         message: "OTP verified successfully",
         email: admin.email,
-        id: admin._id,
-        token: token
+        id: admin._id
       });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+
+  //New Password _____________________________________
+
+  newPassword: async (req, res) => {
+    const { email, password, confirmPassword } = req.body;
+    if (password !== confirmPassword) {
+      return res.status(401).json({ message: "password differs" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 6);
+
+    try {
+      const existing = await Admin.findOne({ email });
+      if (existing) {
+        const admin = await Admin.findOneAndUpdate(
+          { email },
+          { password: hashedPassword }
+        );
+
+        return res
+          .status(200)
+          .json({
+            message: "Password created successfully",
+            password: password
+          });
+      }
+
+      res
+        .status(201)
+        .json({ message: "User created successfully", user: newAdmin });
+    } catch (error) {
+      console.error("Error generating OTP:", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  },
+
+  //Login _____________________________________________
+
+  signin: async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      const admin = await Admin.findOne({ email });
+      const hashedPassword = admin.password;
+      if (!admin) {
+        return res
+          .status(404)
+          .send({ message: "please register before signin" });
+      }
+      bcrypt.compare(password, hashedPassword, (err, result) => {
+        if (err) {
+          console.error("Error comparing password:", err);
+          return;
+        }
+
+        if (result) {
+          console.log("Password is correct");
+          // Proceed with login
+          // Generate JWT
+          const token = jwt.sign(
+            { adminId: admin._id },
+            process.env.JWT_SECRET_ADMIN,
+            {
+              expiresIn: "1h"
+            }
+          );
+          return res.status(200).json({
+            message: "admin logged in successfully",
+            email: admin.email,
+            id: admin._id,
+            token: token
+          });
+        } else {
+          res.status(404).send({message:"Password Incorrect"});
+          // Handle wrong password case
+        }
+      });
+
     } catch (error) {
       console.error(error);
       return res.status(500).json({ message: "Internal Server Error" });
@@ -128,8 +172,8 @@ const adminController = {
     const token = req.header("Authorization").replace("Bearer", "");
 
     if (!token) {
-      return res.status(401).send('Access denied');
-  }
+      return res.status(401).send("Access denied");
+    }
 
     try {
       const decoded = jwt.verify(token, JWT_SECRET_ADMIN);
@@ -145,8 +189,8 @@ const adminController = {
   adminPanel: async (req, res) => {
     const admin = await Admin.findById(req.admin.adminId);
     if (!admin) {
-      return res.status(404).send('User not found');
-  }
+      return res.status(404).send("User not found");
+    }
     res.status(200).send({
       email: admin.email,
       password: admin.password
@@ -155,4 +199,3 @@ const adminController = {
 };
 
 export default adminController;
-
--- server/admin/admin-router/admin.router.js
+++ server/admin/admin-router/admin.router.js
@@ -3,9 +3,10 @@ import adminController from "../admin-controller/admin.controller.js";
 
 const adminRouter = express.Router();
 
-adminRouter.post("/signup", adminController.Signup); //Create User & Generate OTP
-adminRouter.post("/verify-otp",adminController.verifyOtp); //Verify OTP
-// userRouter.post("/generate-otp",userController.generateOtp);
+adminRouter.post("/register", adminController.register); //Create User & Generate OTP
+adminRouter.post("/new-password", adminController.newPassword); //New  Password
+adminRouter.post("/verify-admin",adminController.verifyOtp); //Verify OTP
+adminRouter.post("/signin-admin",adminController.signin); //Signin Admin
 // adminRouter.get("/profile", adminController.authenticateJWT, adminController.profile); //Protected route
 
   
